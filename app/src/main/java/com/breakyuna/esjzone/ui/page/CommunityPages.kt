9aa18ebc951e8b9c1be0969e88640d4c66193759
breakyuna <18855640756wcy@gmail.com>
2026-08-29 12:32:24 +0800

    Fix missing ContentScale import

--- app/src/main/java/com/breakyuna/esjzone/ui/page/CommunityPages.kt
+++ app/src/main/java/com/breakyuna/esjzone/ui/page/CommunityPages.kt
@@ -54,6 +54,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
