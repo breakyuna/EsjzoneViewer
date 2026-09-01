7c1420b4d59937596087853c19750c08226f3579
breakyuna <18855640756wcy@gmail.com>
2026-09-01 20:43:46 +0800

    fix: import compose state map for history covers

--- app/src/main/java/com/breakyuna/esjzone/ui/page/HistoryPage.kt
+++ app/src/main/java/com/breakyuna/esjzone/ui/page/HistoryPage.kt
@@ -45,6 +45,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
