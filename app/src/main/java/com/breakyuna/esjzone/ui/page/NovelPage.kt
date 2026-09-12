cc1f3e5919e2dcad884168e9ba6e8f02dd7e60e6
breakyuna <18855640756wcy@gmail.com>
2026-09-12 11:53:23 +0800

    fix(novel): add missing sp import in NovelPage

--- app/src/main/java/com/breakyuna/esjzone/ui/page/NovelPage.kt
+++ app/src/main/java/com/breakyuna/esjzone/ui/page/NovelPage.kt
@@ -86,6 +86,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.breakyuna.esjzone.ui.navigation.rememberAppViewModel
 import com.breakyuna.esjzone.ui.navigation.AppDestination
 import com.breakyuna.esjzone.R
