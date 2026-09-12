a9c5293dad9d796b4ee852e90fbadc0bc09183d1
breakyuna <18855640756wcy@gmail.com>
2026-09-12 20:42:58 +0800

    fix(home): import saveable state API

--- app/src/main/java/com/breakyuna/esjzone/ui/tab/HomeTab.kt
+++ app/src/main/java/com/breakyuna/esjzone/ui/tab/HomeTab.kt
@@ -33,8 +33,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
